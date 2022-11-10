185fb91f7089e15c7c5c63ea595a275ffb93cb1f
Federico Luna <fluna1973@yahoo.com>
2022-11-10 15:53:18 -0600

    add class solver with functions

--- solver.rb
+++ solver.rb
@@ -1,7 +1,28 @@
-# frozen_string_literal: true
+class Solver
+  def factorial(num)
+    if num.negative?
+      'Please use a positive number'
 
-def back_string(string); 
+    elsif [1, 0].include?(num)
+      1
+    else
+      (1..num).reduce(1, :*)
+    end
+  end
+
+  def back_string(string)
     string.reverse
-end
+  end
 
-puts back_string('Federico')
+  def fizz_buzz(number)
+    if (number % 5).zero? && (number % 3).zero?
+      'fizz_buzz'
+    elsif (number % 3).zero?
+      'fizz'
+    elsif (number % 5).zero?
+      'buzz'
+    else
+      number.to_s
+    end
+  end
+end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,21 +2,26 @@
 
 require_relative '../solver'
 
-describe 'factorial test' do
-  it 'factorialize number' do
-    expect(0).to eql(0)
+describe Solver do
+  context 'factorial test' do
+    it 'factorialize number' do
+      expect(Solver.new.factorial(7)).to eql(5040)
+    end
+
+    it 'Factorial of negative number should return error' do
+      expect(Solver.new.factorial(-8)).to eql('Please use a positive number')
+    end
   end
 end
 
 describe 'reverseString "this" to equal "siht"' do
   it 'reverses the string' do
-    expect(back_string('this')).to eql('siht')
+    expect(Solver.new.back_string('this')).to eql('siht')
   end
 end
 
-
-describe 'Testing the Fizz Buzz method'do
-  it 'should return fizz ' do
-    expect(15).to eql('buzz')
+describe 'Testing the Fizz Buzz method' do
+  it 'should return fizz' do
+    expect(Solver.new.fizz_buzz(9)).to eq('fizz')
   end
 end
